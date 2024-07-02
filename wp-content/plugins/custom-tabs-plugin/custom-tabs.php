1dc51a9182623c449f41d0eb8d98aed774b8351a
idanzazati <idanzazati@gmail.com>
2024-07-02 03:18:43 +0300

    add function to load assets so i can use them later when creating the component

---
+++ wp-content/plugins/custom-tabs-plugin/custom-tabs.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Custom Tabs
+ * Description: Displying a component with options page to manage the content
+ * Author: Idan Zazati
+ * Version: 1.0.0
+ * Text Domain: custom-tabs
+ */
+
+ if(!defined('ABSPATH')) {
+  // code for security
+  exit;
+ }
+
+ class CustomTabs {
+  public function __construct()
+  {
+    // add assets
+    add_action('wp_enqueue_scripts', array($this, 'load_assets')); 
+  }
+
+  public function load_assets() 
+  {
+    wp_enqueue_style(
+      'custom-tabs', 
+      plugin_dir_url(__FILE__) . 'css/custom-tabs.css',
+      array(), 
+      '1.0.0', 
+      'all'
+  );
+
+    wp_enqueue_style( 'typekit-fonts', 'https://use.typekit.net/wuz0gtr.css', array(), null );
+
+    wp_enqueue_script(
+      'custom_tabs',
+      plugin_dir_url( __FILE__ ) . 'js/custom-tabs.js',
+      array(),
+      1,
+      true
+    );
+  }
+}
+
+new CustomTabs;
